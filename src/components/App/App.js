38aaf4ba4d692e7e2c3b547166c994e23bf70224
michaelchun <michael.chun03@gmail.com>
2020-01-27 17:04:58 -0500

    Test hiding stub chat component behind signin/auth

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Route } from 'react-router-dom';
+import AuthenticatedRoute from '../AuthenticatedRoute/AuthenticatedRoute';
+import AutoDismissAlert from '../AutoDismissAlert/AutoDismissAlert';
 import DesktopContainer from '../DesktopContainer/DesktopContainer';
 import ChatContainer from '../ChatContainer/ChatContainer';
+import SignIn from '../SignIn/SignIn';
 
 const App = ({ socket }) => {
+  const [user, setUser] = useState(null);
+  const [alerts, setAlerts] = useState([]);
+
+  const clearUser = () => setUser(null);
+
+  const alert = ({ heading, message, variant }) => {
+    setAlerts(() => [...alerts, { heading, message, variant }]);
+  };
+
   return (
     <DesktopContainer>
+      {alerts.map((alert, index) => (
+        <AutoDismissAlert
+          key={index}
+          heading={alert.heading}
+          variant={alert.variant}
+          message={alert.message}
+        />
+      ))}
       <h1>Test</h1>
-      <ChatContainer socket={socket} />
+      <Route
+        exact
+        path="/"
+        render={() => <SignIn alert={alert} setUser={setUser} />}
+      />
+      <AuthenticatedRoute
+        user={user}
+        path="/home"
+        render={() => (
+          <ChatContainer socket={socket} user={user} clearUser={clearUser} />
+        )}
+      />
     </DesktopContainer>
   );
 };
