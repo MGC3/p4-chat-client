c051bd2966b249b6db3d258cf5a39cc2abdbd1ef
michaelchun <michael.chun03@gmail.com>
2020-01-27 20:39:57 -0500

    Use apiUrl variable instead of hardcoding localhost

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import './index.scss';
 import App from './components/App/App';
 import { HashRouter } from 'react-router-dom';
 import io from 'socket.io-client';
-let socket = io.connect('http://localhost:4741');
+import apiUrl from './apiConfig';
+let socket = io.connect(apiUrl);
 
 const appJsx = (
   <HashRouter>
