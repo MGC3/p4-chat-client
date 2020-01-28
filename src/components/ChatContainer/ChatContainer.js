2a0f0b09d171d8e77fbdfef47e85b0cde5fa1083
michaelchun <michael.chun03@gmail.com>
2020-01-28 09:30:59 -0500

    Add stub styles for chat window

--- src/components/ChatContainer/ChatContainer.js
+++ src/components/ChatContainer/ChatContainer.js
@@ -1,6 +1,8 @@
 import React, { useState, useRef } from 'react';
+import styled from 'styled-components';
 import ChatForm from './ChatForm';
 import ChatMessages from './ChatMessages';
+import Draggable from 'react-draggable';
 
 export default function ChatContainer({ socket }) {
   const [messages, setMessages] = useState([]);
@@ -23,14 +25,61 @@ export default function ChatContainer({ socket }) {
   });
 
   return (
-    <div>
-      <h1>ChatContainer Component</h1>
-      <ChatMessages messages={messages} />
-      <ChatForm
-        handleKeyPress={handleKeyPress}
-        inputRef={inputRef}
-        handleClick={handleClick}
-      />
-    </div>
+    <Draggable handle=".chat-app-drag" defaultPosition={{ x: 250, y: -380 }}>
+      <Container>
+        <TitleBarContainer className="chat-app-drag">
+          <Icon>X</Icon>
+          <TitleText>Instant Messenger</TitleText>
+          <CloseIcon>X</CloseIcon>
+        </TitleBarContainer>
+        <h1>ChatContainer Component</h1>
+        <ChatMessages messages={messages} />
+        <ChatForm
+          handleKeyPress={handleKeyPress}
+          inputRef={inputRef}
+          handleClick={handleClick}
+        />
+      </Container>
+    </Draggable>
   );
 }
+
+const Container = styled.div`
+  width: 500px;
+  height: 450px;
+  border: dashed;
+  position: absolute;
+  overflow: scroll;
+`;
+
+const TitleBarContainer = styled.div`
+  width: 100%;
+  height: 32px;
+  border: dashed;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  display: flex;
+  align-items: center;
+`;
+
+const TitleText = styled.p`
+  color: white;
+  /* abstract font into main css typography */
+  font-family: 'Tahoma';
+`;
+
+const Icon = styled.div`
+  /* test styles delete */
+  border: dashed pink;
+  height: 100%;
+  width: 32px;
+  margin-right: 32px;
+`;
+
+const CloseIcon = styled.button`
+  margin-left: auto;
+  /* test styles delete */
+  height: 100%;
+  width: 32px;
+  color: black;
+`;
