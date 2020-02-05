0e02ae97f69afb9b538399fcedcf1ba194af4111
michaelchun <michael.chun03@gmail.com>
2020-02-04 21:13:09 -0500

    Update signup error messaging to let user know screen name may already exist

--- src/components/AutoDismissAlert/messages.js
+++ src/components/AutoDismissAlert/messages.js
@@ -1,9 +1,11 @@
 export default {
-  signUpSuccess: 'Succesfully registered! You\'ve been signed in as well.',
-  signUpFailure: 'Registration failed. Email may be taken, or passwords don\'t match.',
+  signUpSuccess: "Succesfully registered! You've been signed in as well.",
+  signUpFailure: "Email/ScreenName may be taken, or passwords don't match.",
   signInSuccess: 'Welcome!',
-  signInFailure: 'Failed to sign in. Check your email and password and try again.',
+  signInFailure:
+    'Failed to sign in. Check your email and password and try again.',
   signOutSuccess: 'Come back soon!',
   changePasswordSuccess: 'Password changed successfully!',
-  changePasswordFailure: 'Failed to change passwords. Check your old password and try again.'
-}
+  changePasswordFailure:
+    'Failed to change passwords. Check your old password and try again.'
+};
