f0939f97e59dcfbb967ca9548319377057f1ce20
michaelchun <michael.chun03@gmail.com>
2020-02-08 21:56:08 -0500

    Placeholder desktop icons with links working

--- src/components/IconGrid/IconGrid.js
+++ src/components/IconGrid/IconGrid.js
@@ -4,7 +4,30 @@ import styled from 'styled-components';
 const IconGrid = () => {
   return (
     <Container>
-      <h1>IconGrid Component</h1>
+      <Icon>
+        <StyledLink href="https://google.com" target="_blank">
+          <Wrapper>
+            <Image />
+            <Text>Tic Tac Toe</Text>
+          </Wrapper>
+        </StyledLink>
+      </Icon>
+      <Icon>
+        <StyledLink href="https://google.com" target="_blank">
+          <Wrapper>
+            <Image />
+            <Text>Tic Tac Toe</Text>
+          </Wrapper>
+        </StyledLink>
+      </Icon>
+      <Icon>
+        <StyledLink href="https://google.com" target="_blank">
+          <Wrapper>
+            <Image />
+            <Text>Tic Tac Toe</Text>
+          </Wrapper>
+        </StyledLink>
+      </Icon>
     </Container>
   );
 };
@@ -19,4 +42,37 @@ const Container = styled.div`
   border: dashed red;
   position: absolute;
   z-index: 100;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledLink = styled.a`
+  &:hover {
+    text-decoration: inherit;
+  }
+`;
+
+const Icon = styled.div`
+  width: 120px;
+  height: 120px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Image = styled.div`
+  width: 64px;
+  height: 64px;
+  border: dashed pink;
+`;
+
+const Text = styled.p`
+  margin: 0;
+  color: ${props => props.theme.white};
 `;
