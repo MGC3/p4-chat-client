0db80466d94265164e4c8fc0e017f5797d4ba60f
michaelchun <michael.chun03@gmail.com>
2020-02-09 01:18:26 -0500

    Finish desktop icons with real thumbnails and links

--- src/components/IconGrid/IconGrid.js
+++ src/components/IconGrid/IconGrid.js
@@ -1,30 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
+import ttt from '../../images/tic-tac-toe-300x300.png';
+// pixel cat image attrib to:
+// https://www.pngkey.com/detail/u2t4t4o0o0w7u2o0_orange-cat-cat-pixel-art/
+import cat from '../../images/pixel-cat-300x300.png';
+import petlee from '../../images/petlee-300x300.png';
 
 const IconGrid = () => {
   return (
     <Container>
       <Icon>
-        <StyledLink href="https://google.com" target="_blank">
+        <StyledLink
+          href="https://mgc3.github.io/p1-tic-tac-toe-client/"
+          target="_blank"
+        >
           <Wrapper>
-            <Image />
+            <Image background={ttt} />
             <Text>Tic Tac Toe</Text>
           </Wrapper>
         </StyledLink>
       </Icon>
       <Icon>
-        <StyledLink href="https://google.com" target="_blank">
+        <StyledLink
+          href="https://mgc3.github.io/p2-pet-client/"
+          target="_blank"
+        >
           <Wrapper>
-            <Image />
-            <Text>Tic Tac Toe</Text>
+            <Image background={petlee} />
+            <Text>Petly - Pet Weight Management App</Text>
           </Wrapper>
         </StyledLink>
       </Icon>
       <Icon>
-        <StyledLink href="https://google.com" target="_blank">
+        <StyledLink
+          href="https://vigilant-hugle-84a7b7.netlify.com/"
+          target="_blank"
+        >
           <Wrapper>
-            <Image />
-            <Text>Tic Tac Toe</Text>
+            <Image background={cat} />
+            <Text>Orange Cat or Not Orange Cat</Text>
           </Wrapper>
         </StyledLink>
       </Icon>
@@ -39,7 +53,6 @@ const Container = styled.div`
   width: 100vw;
   display: flex;
   flex-direction: column;
-  border: dashed red;
   position: absolute;
   z-index: 100;
 `;
@@ -59,7 +72,8 @@ const StyledLink = styled.a`
 
 const Icon = styled.div`
   width: 120px;
-  height: 120px;
+  min-height: 120px;
+  margin-bottom: 8px;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -69,10 +83,14 @@ const Icon = styled.div`
 const Image = styled.div`
   width: 64px;
   height: 64px;
-  border: dashed pink;
+  margin: 0;
+  background-size: cover;
+  background-image: ${props => `url(${props.background}`});
 `;
 
 const Text = styled.p`
   margin: 0;
+  text-align: center;
+  font-size: 14px;
   color: ${props => props.theme.white};
 `;
